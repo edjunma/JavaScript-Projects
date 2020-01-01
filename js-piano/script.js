60adefedeff1be4a213f0cd93e53650eb263f683
edjunma <edjunma@gmail.com>
2019-12-31 20:35:10 -0500

    finish piano project, added password gen projcet

--- js-piano/script.js
+++ js-piano/script.js
@@ -28,3 +28,16 @@ function playNote(key) {
 		key.classList.remove('active');
 	});
 }
+
+// SOCIAL PANEL JavaScript
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible');
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible');
+});
