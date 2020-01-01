7b54bb3eb431ceced7c40920c73d410a7146f159
edjunma <edjunma@gmail.com>
2020-01-01 04:34:58 -0500

    working on etch scripts

---
+++ etch-a-sketch/etch-a-sketch.js
@@ -0,0 +1,7 @@
+// Select the lements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 50;
+
+// Setup our canvas for drawing
