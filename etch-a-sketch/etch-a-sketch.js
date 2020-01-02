3fc7d48c4fc542ea9dc5ee97fa736c44eba9dda2
edjunma <edjunma@gmail.com>
2020-01-02 04:09:36 -0500

    more etch functions, randomized point start, and arrow key event listeners

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -5,13 +5,28 @@ const shakebutton = document.querySelector('.shake');
 const MOVE_AMOUNT = 50;
 
 // Setup our canvas for drawing
+// Make a variable called height and width from the same properties as the canvas
+const { width, height } = canvas;
+
+const x = Math.floor(Math.random() * width);
+const x = Math.floor(Math.random() * height);
+// Create random x and y starting points on the canvas
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
 ctx.beginPath(); // This starts the drawing
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
 // Write a draw function
+
+// Write a handler for the keys
+function handleKey() {
+	console.log('HANDLING KEY');
+}
+
+// Listen for the arrow keys
+window.addEventListener('keydown', handleKey);
