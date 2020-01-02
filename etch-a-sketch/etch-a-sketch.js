d6708124f70b6a309065dec7ac01e2d544e5bb4e
edjunma <edjunma@gmail.com>
2020-01-02 01:01:02 -0500

    setup canvas and drawing calls

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,7 +1,17 @@
-// Select the lements on the page - canvas, shake button
+// Select the elements on the page - canvas, shake button
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
 const MOVE_AMOUNT = 50;
 
 // Setup our canvas for drawing
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // This starts the drawing
+ctx.moveTo(200, 200);
+ctx.lineTo(200, 200);
+ctx.stroke();
+
+// Write a draw function
