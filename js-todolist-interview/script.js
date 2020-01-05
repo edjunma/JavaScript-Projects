6637a3e91072f1a75ea37f2d1042e6ce5a09fb8f
edjunma <edjunma@gmail.com>
2020-01-05 03:47:26 -0500

    working on todolist script

--- js-todolist-interview/script.js
+++ js-todolist-interview/script.js
@@ -1,5 +1,12 @@
-function addListItem() {
-	//
+function addListItem(event) {
+	console.log(event.keyCode);
+	if (event.keyCode == 13) {
+		const value = document.getElementById('input-todo').value;
+		const listContain = document.getElementById('todo-list');
+		const listItem = document.createElement('li');
+		listItem.value = value;
+		listContain.appendChild(listItem);
+	}
 }
 
 document.getElementById('input-todo');
