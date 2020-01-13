d861e7075beb91596755975f5e6968dc64c672f6
edjunma <edjunma@gmail.com>
2020-01-12 20:36:20 -0500

    adding hoverboard project setup files

---
+++ js-hover-board/scripts.js
@@ -0,0 +1,8 @@
+const container =
+document.getElementById('container');
+const SQUARES_NR = 500;
+
+for(let i = 0; i <  SQUARES_NR; i++) {
+  const square = document.createElement('div');
+  square.classList
+}
