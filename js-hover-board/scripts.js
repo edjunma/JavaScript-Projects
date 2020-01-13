af09e37569b0d5773d8288f5d8fc79a894cda35c
edjunma <edjunma@gmail.com>
2020-01-12 20:49:59 -0500

    adding event listeners and color functions to hoverboard project

--- js-hover-board/scripts.js
+++ js-hover-board/scripts.js
@@ -1,8 +1,26 @@
-const container =
-document.getElementById('container');
+const container = document.getElementById('container');
 const SQUARES_NR = 500;
 
-for(let i = 0; i <  SQUARES_NR; i++) {
-  const square = document.createElement('div');
-  square.classList
+for (let i = 0; i < SQUARES_NR; i++) {
+	const square = document.createElement('div');
+	square.classList.add('square');
+
+	square.addEventListener('mouseover', () => {
+		setColorToEl(square);
+	});
+
+	square.addEventListener('mouseout', () => {
+		removeColorFromEl(square);
+	});
+
+	container.appendChild(square);
+}
+
+function setColorToEl(element) {
+	const color = getRandomColor();
+	element.style.background = color;
+}
+
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * colors.length)];
 }
