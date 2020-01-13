31f3d30c8aed735dab6eca9668a859477aee6553
edjunma <edjunma@gmail.com>
2020-01-12 21:06:00 -0500

    finish js hover board project

--- js-hover-board/scripts.js
+++ js-hover-board/scripts.js
@@ -1,4 +1,5 @@
 const container = document.getElementById('container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const SQUARES_NR = 500;
 
 for (let i = 0; i < SQUARES_NR; i++) {
@@ -19,8 +20,27 @@ for (let i = 0; i < SQUARES_NR; i++) {
 function setColorToEl(element) {
 	const color = getRandomColor();
 	element.style.background = color;
+	element.style.boxShadow = `0 0 2px ${color} 0 0 10px ${color}`;
+}
+
+function removeColorFromEl(element) {
+	element.style.background = '#1d1d1d';
+	element.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
 	return colors[Math.floor(Math.random() * colors.length)];
 }
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible');
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible');
+});
