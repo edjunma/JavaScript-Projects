c65b6bddc12a2640a0865d54842ca1faf02ee921
edjunma <edjunma@gmail.com>
2020-01-13 01:46:03 -0500

    finish on winning message and tic tac toe functionality, working on restart button

--- js-tic-tac-toe/scripts.js
+++ js-tic-tac-toe/scripts.js
@@ -1,11 +1,30 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+];
 const cellElements = document.querySelectorAll('[data-cell');
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text');
 let circleTurn;
 
-cellElements.forEach(cell => {
-	cell.addEventListener('click', handleClick, { once: true });
-});
+startGame();
+
+function startGame() {
+	circleTurn = false;
+	cellElements.forEach(cell => {
+		cell.addEventListener('click', handleClick, { once: true });
+	});
+	setBoardHoverClass();
+}
 
 function handleClick(e) {
 	// placeMark
@@ -13,11 +32,24 @@ function handleClick(e) {
 	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
 	placeMark(cell, currentClass);
 	// Check For Win
+	if (checkWin(currentClass)) {
+		endGame(false);
+		// Check For Draw
+	} else if (isDraw()) {
+		endGame(true);
+	} else {
+		// Switch Turns
+		swapTurns();
+		setBoardHoverClass();
+	}
+}
 
-	// Check For Draw
-
-	// Switch Turns
-	swapTurns();
+function endGame(draw) {
+	if (draw) {
+	} else {
+		winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+	}
+	winningMessageTextElement.classList.add('show');
 }
 
 function placeMark(cell, currentClass) {
@@ -26,4 +58,23 @@ function placeMark(cell, currentClass) {
 
 function swapTurns() {
 	circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+	board.classList.remove(X_CLASS);
+	board.classList.remove(CIRCLE_CLASS);
+
+	if (circleTurn) {
+		board.classList.add(CIRCLE_CLASS);
+	} else {
+		board.classList.add(X_CLASS);
+	}
+}
+
+function checkWin(currentClass) {
+	return WINNING_COMBINATIONS.some(combination => {
+		return combination.every(index => {
+			return cellElements[index].classList.contains(currentClass);
+		});
+	});
 }
