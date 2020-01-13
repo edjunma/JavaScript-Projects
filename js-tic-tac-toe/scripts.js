6fcc4f37bd426c09f859d0a7585837fa9495f0e3
edjunma <edjunma@gmail.com>
2020-01-13 01:56:33 -0500

    add title, finish restart button, finish tic-tac-toe project

--- js-tic-tac-toe/scripts.js
+++ js-tic-tac-toe/scripts.js
@@ -14,16 +14,23 @@ const cellElements = document.querySelectorAll('[data-cell');
 const board = document.getElementById('board');
 const winningMessageElement = document.getElementById('winningMessage');
 const winningMessageTextElement = document.querySelector('[data-winning-message-text');
+const restartButton = document.getElementById('restartButton');
 let circleTurn;
 
 startGame();
 
+restartButton.addEventListener('click', startGame);
+
 function startGame() {
 	circleTurn = false;
 	cellElements.forEach(cell => {
+		cell.classList.remove(X_CLASS);
+		cell.classList.remove(CIRCLE_CLASS);
+		cell.removeEventListener('click', handleClick);
 		cell.addEventListener('click', handleClick, { once: true });
 	});
 	setBoardHoverClass();
+	winningMessageElement.classList.remove('show');
 }
 
 function handleClick(e) {
