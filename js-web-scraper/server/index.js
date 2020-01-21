341f1798a2b70bb40c6e225be4dae49f1ad0572b
Edmond <edjunma@gmail.com>
2020-01-21 01:39:11 -0500

    finish api setup

--- js-web-scraper/server/index.js
+++ js-web-scraper/server/index.js
@@ -6,3 +6,28 @@ const bodyParser = require('body-parser');
 
 const scrapers = require('./scrapers');
 const db = require('./db')
+
+// USE
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+// GET
+app.get('/creators', async (req, res) => {
+    const creators = await db.getAllCreators();
+    res.send(creators)
+})
+
+// POST
+app.post('/creators', async (req, res) => {
+    console.log(req.body)
+    const channelData = await scrapers.scrapeChannel(req.body.channelURL)
+    const creators = await db.insertCreator(channelData.name, channelData.avatarURL, req.body.channelURL)
+    res.send(creators);
+})
+
+// LISTEN
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
