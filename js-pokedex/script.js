f5e3a7131b654ac54ec56641f922ab657215485c
Edmond <edjunma@gmail.com>
2020-01-20 23:05:20 -0500

    setup css files and social panels

--- js-pokedex/script.js
+++ js-pokedex/script.js
@@ -4,14 +4,28 @@ const pokemons_number = 150;
 fetchPokemons();
 
 const fetchPokemons = async () => {
-    for(let i = 1; i <= pokemons_number; i++) {
-        await getPokemon(i);
-    }
-}
+	for (let i = 1; i <= pokemons_number; i++) {
+		await getPokemon(i);
+	}
+};
 
 const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const res = await fetch(url);
-    const pokemon = await res.json();
-    console.log(pokemon);
-}
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const pokemon = await res.json();
+	createPokemonCard(pokemon);
+};
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
