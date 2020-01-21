391d72c50b4301b53f41dbdd2752a275a1f47382
Edmond <edjunma@gmail.com>
2020-01-20 23:42:47 -0500

    finish script, finish styles

--- js-pokedex/script.js
+++ js-pokedex/script.js
@@ -21,8 +21,31 @@ const createPokemonCard(pokemon) {
     const poke_types = pokemon.types.map(type => type.type.name);
 	const type = main_types.find(type => poke_types.indexOf(type) > -1);
 	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-	const color = colors[type];
+    const color = colors[type];
+    
+    pokemonEl.style.backgroundColor = color;
+
+    const pokeInnerHTML = `
+        <div class="img-container">
+            <img src="https://pokeres.bastionbot.org/images/pokemon/${
+							pokemon.id
+						}.png" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id
+							.toString()
+							.padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `;
+
+	pokemonEl.innerHTML = pokeInnerHTML;
+
+	poke_container.appendChild(pokemonEl);
 }
+
+fetchPokemons();
 
 
 // SOCIAL PANEL JS
