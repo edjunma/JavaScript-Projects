5af5854fbe155e09f905b6dad2dfd2c2539e52c1
Edmond <edjunma@gmail.com>
2020-01-23 03:45:38 -0500

    1/23, add promise function to get creators

--- js-web-scraper/server/database.js
+++ js-web-scraper/server/database.js
@@ -11,6 +11,43 @@ class Creator {
 
 const EntitySchema = require("typeorm").EntitySchema; 
 
+const CreatorSchema = new EntitySchema({
+    name: "Creator",
+    target: Creator,
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true
+        },
+        name: {
+            type: "varchar"
+        },
+        img: {
+            type: "text"
+        },
+        ytURL: {
+            type: "text"
+        }
+    }
+});
+
+async function getConnection() {
+    return await typeorm.createConnection({
+        type: "mysql",
+        host: "localhost",
+        port: 3306,
+        username: "root",
+        password: "password",
+        database: "setuptourist",
+        synchronize: true,
+        logging: false,
+        entities: [
+            CreatorSchema
+        ]
+    })
+}
+
 module.exports = {
     getAllCreators,
     insertCreator
