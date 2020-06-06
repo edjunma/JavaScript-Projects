a601ce29f723bf482b88dc2fdbbf956091d4c656
edjunma <edjunma@gmail.com>
2020-06-06 02:22:15 -0400

    add play, pause, stop button functionality

--- text-to-speech/script.js
+++ text-to-speech/script.js
@@ -8,8 +8,27 @@ playButton.addEventListener('click', () => {
 	playText(textInput.value);
 });
 
+pauseButton.addEventListener('click', pauseText);
+stopButton.addEventListener('click', stopText);
+
 function playText(text) {
+	if (speechSynthesis.paused && speechSynthesis.speaking) {
+		return speechSynthesis.resume();
+	}
 	const utterance = new SpeechSynthesisUtterance(text);
 	utterance.rate = speedInput.value || 1;
+	utterance.addEventListener('end', () => {
+		textInput.disabled = false;
+	});
+	textInput.disabled = true;
 	speechSynthesis.speak(utterance);
+}
+
+function pauseText() {
+	if (speechSynthesis.speaking) speechSynthesis.pause();
+}
+
+function stopText() {
+	speechSynthesis.resume();
+	speechSynthesis.cancel();
 }
