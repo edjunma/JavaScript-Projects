83475c9f221ada56718100ae3a109def3abed378
edjunma <edjunma@gmail.com>
2020-06-07 03:23:12 -0400

    finish randomize food, setup grid script

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -29,3 +29,22 @@ export function draw(gameBoard) {
 export function expandSnake(amount) {
 	newSegments += amount;
 }
+
+export function onSnake(position, { ignoreHead = false } = {}) {
+	return snakeBody.some((segment, index) => {
+		if (ignoreHead && index === 0) return false;
+		return equalPositions(segment, position);
+	});
+}
+
+function equalPositions(pos1, pos2) {
+	return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+	for (let i = 0; i < newSegments; i++) {
+		snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+	}
+
+	newSegments = 0;
+}
